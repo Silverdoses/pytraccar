73aa34243c213ccd587e1f0f7419b83893c80093
Legacier <silverdoses@gmail.com>
2018-12-20 02:18:02 -0500

    Fix tests - Now by routes

--- tests/test_api_calls.py
+++ tests/test_api_calls.py
@@ -16,6 +16,20 @@ invalid_token = 'ThisIsNotAValidToken'
 test_url = 'http://127.0.0.1:8082'
 
 
+@pytest.fixture(scope='module')
+def admin_session():
+    admin = api.TraccarAPI(base_url=test_url)
+    admin.login_with_token(token=admin_token)
+    return admin
+
+
+@pytest.fixture(scope='module')
+def user_session():
+    user = api.TraccarAPI(base_url=test_url)
+    user.login_with_token(token=user_token)
+    return user
+
+
 def test_valid_login_with_credentials():
     user = api.TraccarAPI(base_url=test_url)
     result = user.login_with_credentials(username, correct_password)
@@ -39,55 +53,29 @@ def test_invalid_token_exception():
         user.login_with_token(token=invalid_token)
 
 
-def test_admin_get_all_devices():
-    admin = api.TraccarAPI(base_url=test_url)
-    admin.login_with_token(token=admin_token)
-    result = admin.get_all_devices()
-    assert type(result) == list
+def test_api_users(admin_session, user_session):
+    user, admin = user_session, admin_session
 
-
-def test_user_get_all_devices():
+    # Test not enough permissions
     with pytest.raises(UserPermissionException):
-        user = api.TraccarAPI(base_url=test_url)
-        user.login_with_token(token=user_token)
         user.get_all_devices()
 
-
-def test_create_device():
-    user = api.TraccarAPI(base_url=test_url)
-    user.login_with_token(token=admin_token)
-    result = user.create_device(name='Test Device', unique_id='testdevice')
-    assert type(result) == dict
-
-
-def test_create_duplicated_device():
-    with pytest.raises(ObjectAlreadyExistsException):
-        user = api.TraccarAPI(base_url=test_url)
-        user.login_with_token(token=admin_token)
-        user.create_device(name='Test Device', unique_id='testdevice')
-
-
-def test_get_devices():
-    user = api.TraccarAPI(base_url=test_url)
-    user.login_with_token(token=admin_token)
-    result_by_user_id = user.get_devices()
-    result_by_id = user.get_devices(query='id', params=[1])
-    result_by_unique_id = user.get_devices(query='uniqueId', params=['testdevice'])
-
-    assert type(result_by_user_id) == list
-    assert type(result_by_id) == list
-    assert type(result_by_unique_id) == list
-
-
-def test_device_does_not_exist():
+    # Test non-existent device
     with pytest.raises(ObjectNotFoundException):
-        user = api.TraccarAPI(base_url=test_url)
-        user.login_with_token(token=admin_token)
         user.get_devices(query='uniqueId', params=['NotADevice'])
 
+    task1 = admin.get_all_devices()
+    task2 = user.create_device(name='Test Device', unique_id='testdevice')
+    task3 = user.get_devices()
+    task4 = user.get_devices(query='id', params=[1])
+    task5 = user.get_devices(query='uniqueId', params=['testdevice'])
 
-def test_update_device():
-    user = api.TraccarAPI(base_url=test_url)
-    user.login_with_token(token=admin_token)
-    result = user.update_device(device_id=1, name='ANewName')
-    assert type(result) == list
+    assert type(task1) == list
+    assert type(task2) == dict
+    assert type(task3) == list
+    assert type(task4) == list
+    assert type(task5) == list
+
+    # Test duplicated device
+    with pytest.raises(ObjectAlreadyExistsException):
+        user.create_device(name='Test Device', unique_id='testdevice')
